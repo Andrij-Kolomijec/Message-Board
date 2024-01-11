2fb69d87c8b5de5cc751b5f4d3e699535dd1483a
Difficins <a.kolomijec@seznam.cz>
2024-01-11 12:55:26 +0100

    Can add new messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini Message Board", messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Add a message" });
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+  console.log(req.body.message);
+  res.redirect("/", { title: "Mini Message Board", messages });
 });
 
 module.exports = router;
--- routes/new.js
+++ routes/new.js
@@ -2,8 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+// router.get("/new", function (req, res, next) {
+//   res.render("form");
+// });
 
-module.exports = router;
+// router.post("/new", function (req, res, next) {
+//   message.push({
+//     text: req.body.message,
+//     user: req.body.author,
+//     added: new Date(),
+//   });
+//   res.redirect("index", { title: "Mini Message Board", messages });
+// });
+
+// module.exports = router;
